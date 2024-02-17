2070e305244c1f779c1707bf564f2f5696ded65c
SerkanIT <157528566+SerkanIT@users.noreply.github.com>
2024-02-17 12:48:24 +0500

    flamingo

---
+++ script.js
@@ -0,0 +1,13 @@
+// let name123 = prompt("Write Your Name");
+
+// console.log(name123);
+
+// increment -> ++ , decrement -> --
+let first = parseInt(prompt("Write first number"));
+let second = parseInt(prompt("Write a second number"));
+
+console.log(++first); 
+console.log(--second); 
+
+
+console.log(first + "+"+ second +"=" + (first + second));  
